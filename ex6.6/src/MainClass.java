e3c78d87ef1380fb985813d77294678b4b675647
DESKTOP-UNBAHQ3\porfirie <porfirie.catruna@gmail.com>
2023-10-14 19:58:05 +0300

    exercitiu 6.6

---
+++ ex6.6/src/MainClass.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		BigInteger nrTotal= new BigInteger("1");
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Introduce-ti nr de casute de sah");
+		int nr_casute = scanner.nextInt();
+		
+		
+		
+			 nrTotal = nrTotal.multiply(BigInteger.valueOf(2).pow(nr_casute-1));
+		 
+		
+		System.out.println("Nr de boabe de orez pe ultima casuta este "+ nrTotal);
+		
+		
+		
+		
+
+	}
+
+}
