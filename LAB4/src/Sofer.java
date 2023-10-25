2be8b1a99382a125047ccfe6f4fd018169dc9217
DESKTOP-UNBAHQ3\porfirie <porfirie.catruna@gmail.com>
2023-10-25 19:50:57 +0300

    lab4

--- LAB4/src/Sofer.java
+++ LAB4/src/Sofer.java
@@ -1,4 +1,30 @@
-
 public class Sofer {
+private String nume;
+private String prenume;
+private int varsta;
+private int nrPermis;
+
+public Sofer(String name,String prenume,int varsta,int nr_permis) {
+	        this.nume=nume;
+			this.prenume=prenume;
+			this.varsta=varsta;
+			this.nrPermis=nr_permis;
+}
+
+public String getNume() {
+	return nume;
+}
+public String getPreNume() {
+	return prenume;
+}
+public int getVarsta() {
+	return varsta;
+}
+public int getNrPermis() {
+	return nrPermis;
+}
+
+
+
 
 }
